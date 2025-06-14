d07a85e24362744bc7fae382f6c92d286ff5c5c9
MSD336 <msd336@drexel.edu>
2025-06-14 19:52:33 -0400

    nav bar complete, burger menu for mobile.

---
+++ portfolio/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.getElementById("burger");
+  const navLinks = document.getElementById("nav-links");
+  burger.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
+  });
+});
