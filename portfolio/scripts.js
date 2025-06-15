b57479eeb779480d92106eedfb8efb0481eb70ac
MSD336 <msd336@drexel.edu>
2025-06-14 21:16:17 -0400

    temporarily removed aos

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -1,8 +1,3 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
-
 
 document.addEventListener("DOMContentLoaded", function () {
   const burger = document.getElementById("burger");
